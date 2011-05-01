b31d5f5dc4324084f17638dbcdeccc5e6027ab0b
Alexis Bernard <alexis@obloh.com>
2011-05-01 18:04:09 +0200

    Refactor store operation.

--- lib/dabcup/operation/store.rb
+++ lib/dabcup/operation/store.rb
@@ -3,17 +3,27 @@ module Dabcup
     class Store < Base
       def run(args)
         @profile.dump(dump_path)
+        copy_dump_to_main_storage
+        copy_dump_to_spare_storage
+      ensure
+        path = local_dump_path || dump_path
+        File.delete(path) if remove_local_dump? && File.exists?(path)
+      end
+
+      private
+
+      def copy_dump_to_main_storage
         unless @main_storage.exists?(dump_name)
           retrieve_dump_from_remote_database if retrieve_dump_from_remote_database?
           @main_storage.put(dump_path, dump_name)
         end
-        if @spare_storage
-          @main_storage.get(dump_name, local_dump_path) if not File.exists?(local_dump_path)
-          @spare_storage.put(local_dump_path, dump_name) if not @spare_storage.exists?(dump_name)
+      end
+
+      def copy_dump_to_spare_storage
+        if @spare_storage && !@spare_storage.exists?(dump_name)
+          @main_storage.get(dump_name, local_dump_path) unless File.exists?(local_dump_path)
+          @spare_storage.put(local_dump_path, dump_name) unless @spare_storage.exists?(dump_name)
         end
-      ensure
-        path = local_dump_path || dump_path
-        File.delete(path) if remove_local_dump? and File.exists?(path)
       end
 
       def dump_name
@@ -28,12 +38,12 @@ module Dabcup
         File.exists?(dump_path) ? dump_path : File.join(best_local_dumps_path, dump_name)
       end
 
-      def retrieve_dump_from_remote_database?
-        @profile.via_ssh? && !same_ssh_as_database?(@main_storage)
-      end
-
       def retrieve_dump_from_remote_database
         Storage::Driver.build(@profile.tunnel.to_s + '/tmp').get(dump_name, local_dump_path)
+      end
+
+      def retrieve_dump_from_remote_database?
+        @profile.via_ssh? && !same_ssh_as_database?(@main_storage)
       end
     end
   end
