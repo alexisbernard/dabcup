93c92ca881abfd11edb4ede5951dc4cab0455bc4
Alexis Bernard <alexis@obloh.com>
2011-05-01 14:00:14 +0200

    Keep refactoring duplicated code.

--- lib/dabcup/operation/store.rb
+++ lib/dabcup/operation/store.rb
@@ -2,36 +2,18 @@ module Dabcup
   module Operation
     class Store < Base
       def run(args)
-        @profile.via_ssh? ? dump_with_ssh(args) : dump_without_ssh(args)
-      end
-
-      def dump_without_ssh(args)
-        local_dump_path = nil
+        if @profile.via_ssh? && !same_ssh_as_database?(@main_storage)
+          raise Dabcup::Error.new("Main storage must be on the same host than the database")
+        end
         @profile.dump(dump_path)
         @main_storage.put(dump_path, dump_name) if not @main_storage.exists?(dump_name)
         if @spare_storage
-          local_dump_path = File.exists?(dump_path) ? dump_path : File.join(best_local_dumps_path, dump_name)
           @main_storage.get(dump_name, local_dump_path) if not File.exists?(local_dump_path)
           @spare_storage.put(local_dump_path, dump_name) if not @spare_storage.exists?(dump_name)
         end
       ensure
-        local_dump_path ||= dump_path
-        File.delete(local_dump_path) if remove_local_dump? and File.exists?(local_dump_path)
-      end
-
-      def dump_with_ssh(args)
-        local_dump_path = nil
-        raise Dabcup::Error.new("Main storage must be on the same host than the database") if not same_ssh_as_database?(@main_storage)
-        @profile.dump(dump_path)
-        @main_storage.put(dump_path, dump_name) if not @main_storage.exists?(dump_name)
-        if @spare_storage
-          local_dump_path = File.exists?(dump_path) ? dump_path : File.join(best_local_dumps_path, dump_name)
-          @main_storage.get(dump_name, local_dump_path) if not File.exists?(local_dump_path)
-          @spare_storage.put(local_dump_path, dump_name) if not @spare_storage.exists?(dump_name)
-        end
-      ensure
-        local_dump_path ||= dump_path
-        File.delete(local_dump_path) if remove_local_dump? and File.exists?(local_dump_path)
+        path = local_dump_path || dump_path
+        File.delete(path) if remove_local_dump? and File.exists?(path)
       end
 
       def dump_name
@@ -40,6 +22,10 @@ module Dabcup
 
       def dump_path
         @dump_path ||= File.join(best_dumps_path, dump_name)
+      end
+
+      def local_dump_path
+        File.exists?(dump_path) ? dump_path : File.join(best_local_dumps_path, dump_name)
       end
     end
   end
